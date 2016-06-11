696f4016c74a96963b5311bf5efa279eaf124f65
Simon Funke <simon@simula.no>
2016-06-11 10:52:49 +0000

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Simon Funke',
+      author_email='simon@simula.no',
+      url='http://www.ivmech.com',
+      packages=['ivPID'],
+     )
